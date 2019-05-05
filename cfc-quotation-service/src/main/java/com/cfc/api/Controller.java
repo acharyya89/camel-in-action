b243f52cd6b8abc7c05c07424962dc133d816e9a
mrinmaysantra <mrinmaysantra@mrinmays-air.lan>
2019-05-05 15:30:18 +0100

    Committed after code re-factoring.

--- cfc-quotation-service/src/main/java/com/cfc/api/Controller.java
+++ cfc-quotation-service/src/main/java/com/cfc/api/Controller.java
@@ -3,11 +3,13 @@ package com.cfc.api;
 import java.io.IOException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cfc.pojo.Quote;
+import com.cfc.pojo.QuoteRequest;
 import com.cfc.service.QuoteService;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
@@ -18,9 +20,10 @@ public class Controller {
 	@Autowired
 	QuoteService quoteService;
 
-	@RequestMapping(value = "/get/quote", method = RequestMethod.GET)
-	public Quote getQuote() throws JsonParseException, JsonMappingException, IOException {
-		return quoteService.getQuote();
+	@RequestMapping(value = "/quote/get", method = RequestMethod.POST)
+	public Quote getQuote(@RequestBody QuoteRequest request)
+			throws JsonParseException, JsonMappingException, IOException {
+		return quoteService.getQuote(request);
 	}
 
 }
--- cfc-quotation-service/src/main/java/com/cfc/service/QuoteService.java
+++ cfc-quotation-service/src/main/java/com/cfc/service/QuoteService.java
@@ -6,13 +6,15 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.ResourceUtils;
 
 import com.cfc.pojo.Quote;
+import com.cfc.pojo.QuoteRequest;
 import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class QuoteService {
-	public Quote getQuote() throws JsonParseException, JsonMappingException, IOException {
+	public Quote getQuote(QuoteRequest request) throws JsonParseException, JsonMappingException, IOException {
+		// request ???
 		return new ObjectMapper().readValue(ResourceUtils.getFile("classpath:quote.json"), Quote.class);
 	}
 }
