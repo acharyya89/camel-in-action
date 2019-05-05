e59e614f65d16265677004907d99548233cd7a52
mrinmaysantra <mrinmaysantra@mrinmays-air.lan>
2019-05-05 12:42:24 +0100

    Committed after re-structuring JSON file

--- cfc-quotation-service/src/main/java/com/cfc/service/QuoteService.java
+++ cfc-quotation-service/src/main/java/com/cfc/service/QuoteService.java
@@ -1,9 +1,9 @@
 package com.cfc.service;
 
-import java.io.File;
 import java.io.IOException;
 
 import org.springframework.stereotype.Service;
+import org.springframework.util.ResourceUtils;
 
 import com.cfc.pojo.Quote;
 import com.fasterxml.jackson.core.JsonParseException;
@@ -13,6 +13,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class QuoteService {
 	public Quote getQuote() throws JsonParseException, JsonMappingException, IOException {
-		return new ObjectMapper().readValue(new File("quote.json"), Quote.class);
+		return new ObjectMapper().readValue(ResourceUtils.getFile("classpath:quote.json"), Quote.class);
 	}
 }
