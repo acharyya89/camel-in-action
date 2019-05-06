74206ecbd0062b7dfc2ce891b9e141cc5d0a3e31
mrinmaysantra <mrinmaysantra@mrinmays-air.lan>
2019-05-06 11:18:15 +0100

    Committing after code refactor

---  fce-integration-service/src/main/java/com/fce/api/RestQuoteService.java
+++  fce-integration-service/src/main/java/com/fce/api/RestQuoteService.java
@@ -1,17 +1,17 @@
 package com.fce.api;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.fce.pojo.QuoteResponse;
 import com.fce.pojo.QuoteRequest;
+import com.fce.pojo.QuoteResponse;
 import com.fce.util.AppConstants;
 
 @RequestMapping(AppConstants.URL_ROOT)
 public interface RestQuoteService {
 
-	@GetMapping(AppConstants.URL_GET)
+	@PostMapping(AppConstants.URL_GET)
 	public QuoteResponse getQuote(@RequestBody QuoteRequest request) throws Exception;
 
 }
